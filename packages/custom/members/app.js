5fdcd35541b0495299d0167b666a661af220c372
gpasala <ganesh.pasala@nextsphere.com>
2015-02-26 14:31:50 +0530

    modified for login passport authentication

--- packages/custom/members/app.js
+++ packages/custom/members/app.js
@@ -7,46 +7,52 @@ var Module = require('meanio').Module;
 
 var Members = new Module('members');
 
+
 /*
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
  */
-Members.register(function(app, auth, database) {
+Members.register(function(app, auth, database, passport) {
 
-  //We enable routing. By default the Package Object is passed to the routes
-  Members.routes(app, auth, database);
+    // This is for backwards compatibility
+    Members.auth =require('./authorization');
+    require('./passport')(passport);
 
-  //We are adding a link to the main menu for all authenticated users
-  Members.menus.add({
-    title: 'members',
-    link: 'members',
-    roles: ['authenticated'],
-    menu: 'main'
-  });
-  
-  Members.aggregateAsset('css', 'members.css');
 
-  /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    Members.settings({
+    //We enable routing. By default the Package Object is passed to the routes
+    Members.routes(app, auth, database, passport);
+
+    /* //We are adding a link to the main menu for all authenticated users
+     Members.menus.add({
+     title: 'members',
+     link: 'members',
+     roles: ['authenticated'],
+     menu: 'main'
+     });*/
+
+    Members.aggregateAsset('css', 'members.css');
+
+    /**
+     //Uncomment to use. Requires meanio@0.3.7 or above
+     // Save settings with callback
+     // Use this for saving data from administration pages
+     Members.settings({
         'someSetting': 'some value'
     }, function(err, settings) {
         //you now have the settings object
     });
 
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    Members.settings({
+     // Another save settings example this time with no callback
+     // This writes over the last settings.
+     Members.settings({
         'anotherSettings': 'some value'
     });
 
-    // Get settings. Retrieves latest saved settigns
-    Members.settings(function(err, settings) {
+     // Get settings. Retrieves latest saved settigns
+     Members.settings(function(err, settings) {
         //you now have the settings object
     });
-    */
+     */
 
-  return Members;
+    return Members;
 });
